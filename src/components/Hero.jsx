767f68dd1d525fc10918af0a4a2bda6c2704d970
medkhair <pmedkhair@gmail.com>
2025-11-23 17:13:49 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,30 +2,40 @@ import Inner from "./Inner";
 
 import heroImage from '../images/heroImage5.png';
 
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 function Hero({person}){
     //{person.pictures[0]}
 
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, i'm " + person.lastName + " " + person.firstName,
+        ],
+        loop: true,
+        delaySpeed: 5000,
+    });
+
     return (
-
-
         <>
         
-        <section id="hero" className="main">
-            <Inner className="hero">
-                <div>
-                    <h1>Hi, i'm {person.lastName} <br /> {person.firstName} </h1>
-                    <h3>{person.title}</h3>
-                    <p>{person.description}</p>
-                    <div className="buttons">
-                        <a href="#about" className="button primary">About Me</a>
-                        <a href="#projects" className="button secondary">Projects</a>
+            <section id="hero" className="main">
+                <Inner className="hero">
+                    <div>
+                        <h1>{text} <span><Cursor /></span></h1>
+                        <h3>{person.title}</h3>
+                        <p>{person.description}</p>
+                        <div className="buttons">
+                            <a href="#about" className="button primary">View My work</a>
+                            <a href="#projects" className="button secondary">Download CV</a>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <img src={heroImage} alt="Medkhair Logo" />
-                </div>
-            </Inner>
-        </section>
+                    <div>
+                        <div>
+                            <img src={heroImage} alt="Medkhair Logo" />
+                        </div>
+                    </div>
+                </Inner>
+            </section>
 
         </>
 
