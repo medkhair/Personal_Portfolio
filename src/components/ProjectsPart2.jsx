ab9f2ae1b901e427cfc12677a63b3bcff7d703c8
medkhair <pmedkhair@gmail.com>
2026-01-04 02:55:31 +0100

    projects part2 v2

--- src/components/ProjectsPart2.jsx
+++ src/components/ProjectsPart2.jsx
@@ -7,8 +7,20 @@ function ProjectsPart2({projects}) {
 
     const [showLinks, setShowLinks] = useState(false);
 
-    const toggleLinks = () => {
-        setShowLinks(!showLinks);
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
+
+    const [hasChanged, setHasChanged] = useState(false);
+
+    function toggleLinks(id) {
+        setSelectedProjectIndex(id);
+        if(hasChanged === false){
+            setShowLinks(true);
+            setHasChanged(true);
+        }else{
+            setShowLinks(!showLinks);
+        }
+            
+    
     }
 
     return (
@@ -38,27 +50,47 @@ function ProjectsPart2({projects}) {
                                     ))}
                                 </div>
                                 <div className="project-links">
-                                    <button className="show-links-button" onClick={toggleLinks} ><i class="bi bi-arrow-up-right"></i></button>
+                                    <button className="show-links-button" onClick={() =>{ 
+                                            toggleLinks(index)
+                                            if(selectedProjectIndex !== index){
+                                                setShowLinks(true);
+                                            }
+                                        }
+                                    } >
+                                        <i class="bi bi-arrow-up-right"></i>
+                                    </button>
                                     {
-                                        showLinks ? 
+                                        (showLinks && (selectedProjectIndex === index)) ? 
                                             <div className="links-dropdown">
-                                                {
+                                                {/*
                                                     project.githubUrl != "" ?
+                                                    */
                                                         <a href={project.designUrl} target="_blank" rel="noopener noreferrer" className="project-link"><i class="bi bi-github"></i></a>
+                                                        /*
                                                     :
                                                         null
+                                                        */
                                                 }
                                                 {
+                                                    /*
                                                     project.designUrl != "" ?
+
+                                                    */
                                                         <a href={project.liveUrl} target="_blank" rel="noopener noreferrer" className="project-link"><i class="bi bi-palette"></i></a>
-                                                    :
+                                                    /*
+                                                        :
                                                         null
+                                                        */
                                                 }
                                                 {
-                                                    project.liveUrl != "" ?                                     
+                                                    /*
+                                                    project.liveUrl != "" ?        
+                                                    */                             
                                                         <a href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="project-link"><i class="bi bi-globe"></i></a> 
-                                                    : 
+                                                    /*
+                                                        : 
                                                         null
+                                                        */
                                                 }
                                             </div>
                                         : null
