439515e0291ca452f30ee584b3e329234125c535
medkhair <pmedkhair@gmail.com>
2026-01-14 17:28:24 +0100

    fixes v4

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -3,7 +3,7 @@
 function ServiceItem({ service}) {
     return (
         <div className="service-item">
-            <img className={`service-icon ${service.icon}`} src={`../src/images/icons/${service.icon}`} />
+            <img className={`service-icon ${service.icon}`} src={`/images/icons/${service.icon}`} />
             <h3 className="service-title">{service.title}</h3>
             <p className="service-description">{service.description}</p>
         </div>
